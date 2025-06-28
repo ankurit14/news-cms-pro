649f7c7ad53b5f9c149486a353df382cf56470f8
ankur <ankutit14@gmail.com>
2025-06-28 10:07:39 +0530

    feat(news): add news admin module, view and main view

---
+++ models/Comment.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+const commentSchema = new mongoose.Schema({
+    article: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'News',
+        required: true
+    },
+    name: {
+        type:String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['pending', 'approved', 'rejected'],
+        required: true
+    }
+});
+
+module.exports = mongoose.model('Comment', commentScheme);
