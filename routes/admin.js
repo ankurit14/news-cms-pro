b037fabd39f132fc3a8c7292da260a4f20e1ab98
ankur <ankutit14@gmail.com>
2025-07-07 16:59:24 +0530

    worked for router protection

--- routes/admin.js
+++ routes/admin.js
@@ -5,57 +5,58 @@ const userController = require('../controller/userController');
 const categoryController = require('../controller/categoryController');
 const articleController = require('../controller/articleController');
 const commentController = require('../controller/commentController');
+const isLoggedIn = require('../middleware/isLoggedin');
 
 //Login Routes
 router.get('/', userController.loginPage);
 router.post('/index', userController.adminLogin);
 router.get('/logout', userController.logout);
-router.get('/dashboard', userController.dashboard);
-router.get('/settings', userController.settings);
+router.get('/dashboard', isLoggedIn, userController.dashboard);
+router.get('/settings', isLoggedIn, userController.settings);
 
 //User CRUD Routes
 //Display all users
-router.get('/users', userController.allUser);
+router.get('/users', isLoggedIn, userController.allUser);
 //Open Add user page
-router.get('/add-user', userController.addUserPage);
+router.get('/add-user', isLoggedIn, userController.addUserPage);
 //Save Add user page
-router.post('/add-user', userController.addUser);
+router.post('/add-user', isLoggedIn, userController.addUser);
 
 //Open update user page
-router.get('/update-user/:id', userController.updateUserPage);
+router.get('/update-user/:id', isLoggedIn, userController.updateUserPage);
 //Save Update user page
-router.post('/update-user/:id', userController.updateUser);
+router.post('/update-user/:id', isLoggedIn, userController.updateUser);
 //Delete user page
-router.delete('/delete-user/:id', userController.deleteUser);
+router.delete('/delete-user/:id', isLoggedIn, userController.deleteUser);
  
 //Category CRUD routes
 //Display all categories
-router.get('/category', categoryController.allCategory);
+router.get('/category', isLoggedIn, categoryController.allCategory);
 //Open Add category page
-router.get('/add-category', categoryController.addCategoryPage);
+router.get('/add-category', isLoggedIn, categoryController.addCategoryPage);
 //Save Add category page
-router.post('/add-category', categoryController.addCategory);
+router.post('/add-category', isLoggedIn, categoryController.addCategory);
 //Open update category page
-router.get('/update-category/:id', categoryController.updateCategoryPage);
+router.get('/update-category/:id', isLoggedIn, categoryController.updateCategoryPage);
 //Save Update category page
-router.post('/update-category/:id', categoryController.updateCategory);
+router.post('/update-category/:id', isLoggedIn, categoryController.updateCategory);
 //Delete category page
-router.delete('/delete-category/:id', categoryController.deleteCategory);
+router.delete('/delete-category/:id', isLoggedIn, categoryController.deleteCategory);
 
 
 //Article CRUD route
 //Display all article
-router.get('/article', articleController.allArticle);
+router.get('/article', isLoggedIn, articleController.allArticle);
 //Open Add article page
-router.get('/add-article', articleController.addArticlePage);
+router.get('/add-article', isLoggedIn, articleController.addArticlePage);
 //Save Add article page
-router.post('/add-article', articleController.addArticle);
+router.post('/add-article', isLoggedIn, articleController.addArticle);
 //Open update article page
-router.get('/update-article/:id', articleController.updateArticlePage);
+router.get('/update-article/:id', isLoggedIn, articleController.updateArticlePage);
 //Save Update article page
-router.post('/update-article/:id', articleController.updateArticle);
+router.post('/update-article/:id', isLoggedIn, articleController.updateArticle);
 //Delete article page
-router.delete('/delete-article/:id', articleController.deleteArticle);
+router.delete('/delete-article/:id', isLoggedIn, articleController.deleteArticle);
 
 
 //Display all comments
